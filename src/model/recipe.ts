a921c538026fcf8c781839995076d5cb2453b020
Connor Reitz <connorreitz@Connors-MacBook-Pro.local>
2025-03-11 00:12:54 -0400

    post endpoint

--- src/model/recipe.ts
+++ src/model/recipe.ts
@@ -1,15 +1,37 @@
 /**
- * Receipe model for Dynamo
+ * recipe model for Dynamo
  */
 
+import { z } from "zod";
+
 export interface Recipe {
-    ingredients: Array<Ingredient>;
-    steps: string;
     description: string;
+    ingredients: Array<Ingredient>;
+    servings: number;
+    source: string;
+    steps: string;
+    title: string;
+    
 }
 
 export interface Ingredient {
-    name: string
-    amount: number
-    unit: string
+    name: string;
+    amount: number;
+    unit: string;
 }
+
+// validation against post event body
+export const RECIPE_BODY_SCHEMA = z.object({
+    description: z.string(),
+    ingredients: z.array(
+        z.object({
+            name: z.string(),
+            amount: z.number(),
+            unit: z.string()
+        })
+    ).min(1),
+    servings: z.number(),
+    source: z.string(),
+    steps: z.string(),
+    title: z.string()
+})
